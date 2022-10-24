50613919f7a70daa046227970f6acd562843cd99
natha <nathanael.constant@gmail.com>
2022-10-24 12:56:23 +0200

    yo

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,1 +1,23 @@
-console.log('yo')
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+import fs from 'fs';
+
+async function getTransitions() {
+    return await prisma.transition.findMany({
+        select: {
+            Titre: true,
+            Date: true,
+            Heure: true
+        }
+    });
+}
+
+// const getTransitions = async () => 
+    
+
+
+getTransitions().then(data => {
+    fs.writeFileSync('./src/data/transitions.json', JSON.stringify(data, null, 4));
+
+    // console.log(data);
+});
---
+++ engine/src/models/transition.ts
@@ -0,0 +1,5 @@
+export interface Transition {
+    titre: string,
+    date: Date,
+    heure: Date
+}
